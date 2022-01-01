604d696d1934ca4ee3adf1871f4dc99ddf9a0cee
thunderbuster <67307203+thunderbuster@users.noreply.github.com>
2022-01-01 19:00:00 +0530

    Add files via upload

---
+++ rat.py
@@ -0,0 +1,17 @@
+from re import S
+import random
+
+import plotly.figure_factory as ff
+
+result=[]
+
+for i in range(0,100):
+ d1=random.randint(1,6)
+ d2=random.randint(1,6)
+ s=d1+d1
+ result.append(S)
+
+ print(result)
+
+ fig= ff.Create_distplot([result],['rating']),show_his
+ fig.show()
